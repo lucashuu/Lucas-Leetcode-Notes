514e7db5b8bc82eaea0f2632881b90dae782cdb3
LucasHu <51680196+lucashuu@users.noreply.github.com>
2021-07-28 20:54:20 -0400

    Time: 0 ms (100.00%), Space: 39.1 MB (86.02%) - LeetHub

--- kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
+++ kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
@@ -1,28 +1,28 @@
+// 利用
 class Solution {
-    // partition 模板调用
-    // quick sort
-    // 递归结束条件 when  m + 1 == k
     public int findKthLargest(int[] nums, int k) {
-        if(nums.length == 0 || nums == null){
+    //利用快排的思想找第n-k 小的数 
+    //套用九章的快排模板
+        if(nums.length < 1 || nums == null){
             return -1;
         }
-        if(nums.length == 1){
-            return nums[0];
-        }
-        return partition(nums, 0, nums.length - 1, nums.length - k);
+        // 1 2 3 4 5 6
+        // 6 - 2 = 4 
+        int kthElement = partition(nums, 0, nums.length - 1, nums.length - k);
+        return kthElement;
     }
-    public int partition(int[] nums, int start, int end, int k){
+    public int partition (int[] nums, int start, int end, int k){
         if(start >= end){
             return nums[k];
         }
-        int left = start; int right = end;
+        int left = start;
+        int right = end;
         int pivot = nums[(start + (end - start) / 2)];
-        
-        while (left <= right){
+        while(left <= right){
             while(left <= right && nums[left] < pivot){
                 left++;
             }
-            while(right >= left && nums[right] > pivot){
+            while(left <= right && nums[right] > pivot){
                 right--;
             }
             if(left <= right){
@@ -31,19 +31,17 @@ class Solution {
                 right--;
             }
         }
-        if(k <= right ){
-            return partition(nums, start, right, k);
-        }
-        if (k >= left) {
-            return partition(nums,left, end, k);
-        }
-        return nums[k];
+        if(k >= left){
+                    return partition(nums, left, end, k);
+                }
+        if(k <= right){
+                    return partition(nums, start, right, k);
+                }
+            return nums[k];
     }
-    public void swap(int[] nums, int i, int j){
-        int tmp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = tmp;
+    public void swap(int[] nums, int l, int r){
+        int temp = nums[l];
+        nums[l] = nums[r];
+        nums[r] = temp;
     }
-       
-        
-    }
+}
