222af26ca497a368673082693dea72b42649c7aa
LucasHu <51680196+lucashuu@users.noreply.github.com>
2021-07-31 11:54:10 -0400

    Time: 4 ms (55.08%), Space: 41.6 MB (10.39%) - LeetHub

---
+++ combination-sum/combination-sum.java
@@ -0,0 +1,30 @@
+class Solution {
+    List<List<Integer>> res = new ArrayList<>();
+    List<Integer> ans = new ArrayList<>();
+    //和为target的时候ans 加入res
+    //此次combination 可以有重复数据 所以 for 不自增
+    //思路基本和combination 一样
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        if(candidates.length == 0 || candidates == null){
+            return res;
+        }
+        dfs(0, candidates, target);
+        return res;
+    }
+    public void dfs(int pos, int[] nums, int gap){
+        if(gap == 0){
+            res.add(new ArrayList<>(ans));
+            return;
+        }
+        for(int i = pos; i < nums.length; i++){
+            int dif = gap - nums[i];
+            if(dif >= 0){
+                ans.add(nums[i]);
+                dfs(i, nums, dif);
+                ans.remove(ans.size() -1);
+            }else{
+                continue;
+            }
+        }
+    }
+}
