a0f82bfdb807f48b45ed73f9ac166952329de2c0
LucasHu <51680196+lucashuu@users.noreply.github.com>
2021-08-06 13:13:51 -0400

    Time: 2 ms (77.05%), Space: 39.3 MB (51.54%) - LeetHub

--- kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
+++ kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
@@ -1,47 +1,28 @@
-// 利用
 class Solution {
+    //利用 最小堆
+    // 如果堆里面num 的数目 小于 k
+    // 则不断push 进堆
+    // 如果超过k 则检查 如果push 进的数比 堆顶的数大 则 删除堆顶的数 替换成 它 并heapify
+    // 直到走完所有数为止
     public int findKthLargest(int[] nums, int k) {
-    //利用快排的思想找第n-k 小的数 
-    //套用九章的快排模板
-        if(nums.length < 1 || nums == null){
+        if(nums.length == 0){
             return -1;
         }
-        // 1 2 3 4 5 6
-        // 6 - 2 = 4 
-        int kthElement = partition(nums, 0, nums.length - 1, nums.length - k);
-        return kthElement;
-    }
-    public int partition (int[] nums, int start, int end, int k){
-        if(start >= end){
-            return nums[k];
+        PriorityQueue<Integer> pq = new PriorityQueue<>(k, new Comparator<Integer>(){
+            @Override
+            public int compare(Integer i1, Integer i2){
+                return i1 - i2;
+            }
+        });
+        for(int i = 0; i < k; i++){
+            pq.offer(nums[i]);
         }
-        int left = start;
-        int right = end;
-        int pivot = nums[(start + (end - start) / 2)];
-        while(left <= right){
-            while(left <= right && nums[left] < pivot){
-                left++;
-            }
-            while(left <= right && nums[right] > pivot){
-                right--;
-            }
-            if(left <= right){
-                swap(nums, left, right);
-                left++;
-                right--;
+        for(int i = k; i < nums.length; i++){
+            if(nums[i] >= pq.peek()){
+                pq.poll();
+                pq.offer(nums[i]);
             }
         }
-        if(k >= left){
-                    return partition(nums, left, end, k);
-                }
-        if(k <= right){
-                    return partition(nums, start, right, k);
-                }
-            return nums[k];
-    }
-    public void swap(int[] nums, int l, int r){
-        int temp = nums[l];
-        nums[l] = nums[r];
-        nums[r] = temp;
+        return pq.peek();
     }
 }
