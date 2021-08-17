826392d92c2de7248175eaa9c1355b9f3d90ba9e
LucasHu <51680196+lucashuu@users.noreply.github.com>
2021-08-17 15:45:14 -0400

    Time: 5 ms (35.57%), Space: 39.5 MB (41.24%) - LeetHub

--- combination-sum/combination-sum.java
+++ combination-sum/combination-sum.java
@@ -1,30 +1,35 @@
 class Solution {
-    List<List<Integer>> res = new ArrayList<>();
-    List<Integer> ans = new ArrayList<>();
-    //和为target的时候ans 加入res
-    //此次combination 可以有重复数据 所以 for 不自增
-    //思路基本和combination 一样
+    List<List<Integer>> res;
+    List<Integer> ans;
+    
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        if(candidates.length == 0 || candidates == null){
-            return res;
-        }
-        dfs(0, candidates, target);
+        res = new ArrayList<>();
+        ans = new ArrayList<>();
+        Arrays.sort(candidates);
+        dfs(candidates, target, 0);
         return res;
     }
-    public void dfs(int pos, int[] nums, int gap){
-        if(gap == 0){
+    public void dfs(int[] candidates, int target, int Index){
+        if(target == 0){
             res.add(new ArrayList<>(ans));
             return;
         }
-        for(int i = pos; i < nums.length; i++){
-            int dif = gap - nums[i];
-            if(dif >= 0){
-                ans.add(nums[i]);
-                dfs(i, nums, dif);
-                ans.remove(ans.size() -1);
-            }else{
-                continue;
-            }
+        if(target < 0){
+            return;
         }
+        if(target > 0){
+            //对于所有的选择 pos 不自增
+           for(int i = Index; i < candidates.length; i++){
+              
+               ans.add(candidates[i]);
+                
+               dfs(candidates, target - candidates[i], i);
+               
+               ans.remove(ans.size() - 1);
+           }
+            
+        }
+        
+        
     }
 }
